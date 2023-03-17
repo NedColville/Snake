1982b6d4feb4b6f9b3cb68929b236c7ac852b882
NedColville <115794537+NedColville@users.noreply.github.com>
2023-03-17 11:54:32 +0000

    Update main.s

--- main.s
+++ main.s
@@ -1,24 +1,35 @@
-	#include <xc.inc>
+#include <xc.inc>
+
+extrn	GLCD_display_on, GLCD_display_off,GLCD_output, GLCD_sety, GLCD_setx, set_IO, GLCD_pickPageL, send_I, send_D
 
 psect	code, abs
-	
-main:
-	org	0x0
-	goto	start
-
-	org	0x100		    ; Main code starts here at address 0x100
+rst:
+    org	0x0
+    goto start
+; Main code starts here at address 0x100
 start:
-	movlw 	0x0
-	movwf	TRISB, A	    ; Port C all outputs
-	bra 	test
-loop:
-	movff 	0x06, PORTB
-	incf 	0x06, W, A
-test:
-	movwf	0x06, A	    ; Test for end of loop condition
-	movlw 	0x63
-	cpfsgt 	0x06, A
-	bra 	loop		    ; Not yet finished goto start of loop again
-	goto 	0x0		    ; Re-run program from start
-
-	end	main
+    nop
+    org	0x100
+    call set_IO
+    nop
+    call GLCD_display_on
+    call GLCD_pickPageL
+    movlw 0x00
+    call GLCD_sety
+    movlw 00000000B
+    call GLCD_setx
+    movlw 11111111B
+    movwf 0x01
+    movlw 00000100B
+    movwf 0x02
+    
+write2:
+    call write
+    decfsz 0x02
+    bra write2
+write:
+    movlw 0xFF
+    call send_D
+    decfsz 0x01
+    bra write
+    return
